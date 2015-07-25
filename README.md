cf2b0ba2098294ce3d92a726c471b693ba630a5b
Zoltan Kochan <zoltan.kochan@gmail.com>
2015-07-25 15:16:19 +0300

    Unit tests updated.

---
+++ README.md
@@ -0,0 +1,36 @@
+# callback-store
+
+A store for callbacks waiting for delayed response.
+
+[![Dependency Status](https://david-dm.org/zkochan/callback-store/status.svg?style=flat)](https://david-dm.org/zkochan/callback-store)
+[![Build Status](https://travis-ci.org/zkochan/callback-store.svg?branch=master)](https://travis-ci.org/zkochan/callback-store)
+[![npm version](https://badge.fury.io/js/callback-store.svg)](http://badge.fury.io/js/callback-store)
+
+
+## Installation
+
+```
+npm install --save callback-store
+```
+
+
+## Usage
+
+```js
+var CallbackStore = require('callback-store');
+
+var cbStore = new CallbackStore();
+var cid = cbStore.add(function() {
+  console.log('Hello world!');
+});
+
+/* ... */
+
+var cb = cbStore.get(cid);
+cb();
+```
+
+
+## License
+
+The MIT License (MIT)
--- test/index.js
+++ test/index.js
@@ -1,75 +1,70 @@
 'use strict';
 
 var chai = require('chai');
+var sinon = require('sinon');
 var expect = chai.expect;
 var CallbackStore = require('../lib');
-var noop = function () {};
+var noop = function() {};
 
-describe('CallbackStore', function () {
-  it('add throws error if not function passed', function () {
+describe('CallbackStore', function() {
+  it('add throws error if not function passed', function() {
     var callbackStore = new CallbackStore();
-    expect(function () {
+    expect(function() {
         callbackStore.add(14);
       })
       .to.throw(Error, 'Invalid argument passed to CallbackStore.add. Function expected but got `number`.');
   });
 
-  it('returns correct callback', function (done) {
+  it('returns correct callback', function(done) {
     var callbackStore = new CallbackStore();
-    var cId = callbackStore.add(done);
-    var cb = callbackStore.get(cId);
+    var cid = callbackStore.add(done);
+    var cb = callbackStore.get(cid);
     expect(cb).to.be.a('function');
     cb();
   });
 
-  it('returns different correlation IDs', function () {
+  it('returns different correlation IDs', function() {
     var callbackStore = new CallbackStore();
-    var cId1 = callbackStore.add(noop);
-    var cId2 = callbackStore.add(noop);
-    expect(cId1).to.not.equal(cId2);
+    var cid1 = callbackStore.add(noop);
+    var cid2 = callbackStore.add(noop);
+    expect(cid1).to.not.eq(cid2);
   });
 
-  it('returns callback only once', function () {
+  it('returns callback only once', function() {
     var callbackStore = new CallbackStore();
-    var cId = callbackStore.add(noop);
-    callbackStore.get(cId);
-    var nothing = callbackStore.get(cId);
+    var cid = callbackStore.add(noop);
+    callbackStore.get(cid);
+    var nothing = callbackStore.get(cid);
     expect(nothing).to.be.null;
   });
 
-  it('if request timed out the callback function is called with an error', function (done) {
+  it('if request timed out the callback function is called with an error', function(done) {
     var callbackStore = new CallbackStore({
       ttl: 10
     });
-    var cId = callbackStore.add(function (err) {
+    var cid = callbackStore.add(function(err) {
       expect(err.message).to.be.equal('Response timed out.');
-      expect(callbackStore.get(cId)).to.be.null;
+      expect(callbackStore.get(cid)).to.be.null;
       done();
     });
   });
 
-  describe('release', function () {
-    it('should release all waiting callbacks', function (done) {
-      var releasesCount = 0;
+  describe('release', function() {
+    it('should release all waiting callbacks', function() {
+      var spy1 = sinon.spy();
+      var spy2 = sinon.spy();
       var callbackStore = new CallbackStore();
-      var cId1 = callbackStore.add(function (err) {
-        expect(err.message).to.be.equal('Request callback was released.');
-        expect(callbackStore.get(cId1)).to.be.null;
-        releasesCount++;
-        if (releasesCount === 2) {
-          done();
-        }
-      });
-      var cId2 = callbackStore.add(function (err) {
-        expect(err.message).to.be.equal('Request callback was released.');
-        expect(callbackStore.get(cId2)).to.be.null;
-        releasesCount++;
-        if (releasesCount === 2) {
-          done();
-        }
-      });
-      
+      var cid1 = callbackStore.add(spy1);
+      var cid2 = callbackStore.add(spy2);
       callbackStore.releaseAll();
+
+      expect(spy1.calledOnce).to.be.true;
+      expect(spy1.getCall(0).args[0].message).to.be.equal('Request callback was released.');
+      expect(callbackStore.get(cid1)).to.be.null;
+
+      expect(spy2.calledOnce).to.be.true;
+      expect(spy2.getCall(0).args[0].message).to.be.equal('Request callback was released.');
+      expect(callbackStore.get(cid2)).to.be.null;
     });
   });
 });
