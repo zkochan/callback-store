19057ff55bae52d4b06d6795db384a3050140da8
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-07 17:26:01 +0200

    kamikaze updated to 1.0.0

--- test/index.js
+++ test/index.js
@@ -2,18 +2,20 @@
 
 const chai = require('chai')
 const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
 const expect = chai.expect
 const CallbackStore = require('../lib')
+
+chai.use(sinonChai)
+
 function noop() {}
 
 describe('CallbackStore', function() {
   it('add throws error if not function passed', function() {
     let callbackStore = new CallbackStore()
-    expect(function() {
-      callbackStore.add(14)
-    })
-    .to.throw(Error, 'Invalid argument passed to CallbackStore.add. ' +
-      'Function expected but got `number`.')
+    expect(() => callbackStore.add(14))
+      .to.throw(Error, 'Invalid argument passed to CallbackStore.add. ' +
+        'Function expected but got `number`.')
   })
 
   it('returns correct callback', function(done) {
@@ -39,6 +41,32 @@ describe('CallbackStore', function() {
     expect(nothing).to.be.undefined
   })
 
+  describe('ttl', function() {
+    beforeEach(function() {
+      this.clock = sinon.useFakeTimers()
+    })
+
+    afterEach(function() {
+      this.clock.restore()
+    })
+
+    it('should release callback after timeout', function() {
+      let callbackStore = new CallbackStore({
+        ttl: 10,
+      })
+
+      let spy = sinon.spy()
+      let cid = callbackStore.add(spy)
+
+      this.clock.tick(20)
+
+      expect(callbackStore.get(cid)).to.be.undefined
+      expect(spy).to.have.been.calledOnce
+      expect(spy.getCall(0).args[0].message)
+        .to.be.eq('Method execution exceeded the time limit of `10`')
+    })
+  })
+
   describe('release', function() {
     it('should release all waiting callbacks', function() {
       let spy1 = sinon.spy()
@@ -48,27 +76,15 @@ describe('CallbackStore', function() {
       let cid2 = callbackStore.add(spy2)
       callbackStore.releaseAll()
 
-      expect(spy1.calledOnce).to.be.true
+      expect(spy1).to.have.been.calledOnce
       expect(spy1.getCall(0).args[0].message)
         .to.be.eq('Request callback was released.')
       expect(callbackStore.get(cid1)).to.be.undefined
 
-      expect(spy2.calledOnce).to.be.true
+      expect(spy2).to.have.been.calledOnce
       expect(spy2.getCall(0).args[0].message)
         .to.be.eq('Request callback was released.')
       expect(callbackStore.get(cid2)).to.be.undefined
     })
-  })
-
-  it('exec applies the correct arguments and context', function() {
-    let spy = sinon.spy()
-    let callbackStore = new CallbackStore()
-    let cid = callbackStore.add(spy)
-    callbackStore.exec(cid, 1, [2, 3])
-
-    expect(callbackStore.get(cid)).to.be.undefined
-    expect(spy.calledOnce).to.be.true
-    expect(spy.calledOn(1)).to.be.true
-    expect(spy.calledWithExactly(2, 3)).to.be.true
   })
 })
